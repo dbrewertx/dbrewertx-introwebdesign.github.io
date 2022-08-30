86f95a1de892a2f0d0ab3eca70ba9c26fed2a0cf
Douglas Brewer <50505000+dbrewertx@users.noreply.github.com>
2022-08-29 20:52:38 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function showMenu() {
+  var menu = document.getElementById("menuItems");
+  if (menu.style.display=="inline") {
+    menu.style.display="none";
+  } else {
+    menu.style.display="inline";
+  }
+}
+
+function contact() {
+  var name=prompt("What is your name?");
+}
