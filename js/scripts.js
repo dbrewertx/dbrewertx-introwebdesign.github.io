e3c95521ebb1e2486fd765687f52ad3fd82ac600
Douglas Brewer <50505000+dbrewertx@users.noreply.github.com>
2022-08-29 20:22:21 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  var menu = document.getElementById("menuItems");
+  if (menu.style.display=="inline") {
+    menu.style.display="none";
+  } else {
+    menu.style.display="inline";
+  }
+}
